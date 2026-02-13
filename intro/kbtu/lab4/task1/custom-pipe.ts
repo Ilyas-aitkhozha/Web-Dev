374e9f85b967c0f36558c1d84e2ba47fb4290c16
Tsuki <emilatanlike@gmail.com>
2026-02-13 12:25:00 +0500

    Update custom-pipe.ts

--- intro/kbtu/lab4/task1/custom-pipe.ts
+++ intro/kbtu/lab4/task1/custom-pipe.ts
@@ -9,3 +9,19 @@ import {ReversePipe} from './reverse.pipe';
 export class App {
   word = 'You are a champion';
 }
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'reverse',
+})
+export class ReversePipe implements PipeTransform {
+  transform(value: string): string {
+    let reverse = '';
+
+    for (let i = value.length - 1; i >= 0; i--) {
+      reverse += value[i];
+    }
+
+    return reverse;
+  }
+}

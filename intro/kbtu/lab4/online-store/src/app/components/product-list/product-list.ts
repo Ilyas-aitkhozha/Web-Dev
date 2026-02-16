a0448f9d3c7eb1ef919651fac300ec277d64f406
Ilyas Aitkhozha <emilatanlike@gmail.com>
2026-02-16 16:14:37 +0500

    lab4 enchanced

--- intro/kbtu/lab4/online-store/src/app/components/product-list/product-list.ts
+++ intro/kbtu/lab4/online-store/src/app/components/product-list/product-list.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Product } from '../../models/product.model';
 import { PRODUCTS } from '../../models/products';
 import { ProductCard } from '../product-card/product-card';
@@ -7,10 +8,27 @@ import { ProductCard } from '../product-card/product-card';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, ProductCard],
+  imports: [CommonModule, FormsModule, ProductCard],
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.css']
 })
-export class ProductList {
+export class ProductList implements OnInit {
   products: Product[] = PRODUCTS;
+  filteredProducts: Product[] = [];
+  searchQuery: string = '';
+
+  ngOnInit(): void {
+    this.filteredProducts = this.products;
+  }
+
+  filterProducts(): void {
+    const q = this.searchQuery.trim().toLowerCase();
+    if (!q) {
+      this.filteredProducts = this.products;
+      return;
+    }
+    this.filteredProducts = this.products.filter(p =>
+      p.name.toLowerCase().includes(q)
+    );
+  }
 }
